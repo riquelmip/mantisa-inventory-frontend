d8844cde75dd7976e4a270db9271ba491ae2e68f
Riquelmi Palacios <90204956+riquelmip@users.noreply.github.com>
2024-10-21 12:27:05 -0600

    ultimos cambios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../service/api.service';
-import { FormsModule } from '@angular/forms';  
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,46 +7,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [ FormsModule, CommonModule ]
+  imports: [FormsModule, CommonModule],
 })
-export class HomeComponent implements OnInit {
-  databases: string[] = [];
-  selectedDatabase: string = '';
-  mensajeConexion: string = '';
-  testmsg: string = '';
-
-  data: any;
-
-  constructor(private apiService: ApiService) { }
-
-  ngOnInit():void {
-    this.cargarBasesDeDatos(); 
-  }
-
-  async cargarBasesDeDatos() {
-    try {
-      this.databases = await this.apiService.obtenerDatos();
-    } catch (error) {
-      console.error("Error al cargar las bases de datos", error);
-    }
-  }
- 
-  async onSelectDatabase() {
-    if (this.selectedDatabase) {
-      try {
-        this.mensajeConexion = await this.apiService.conectarBaseDeDatos(this.selectedDatabase);
-      } catch (error) {
-        console.error("Error al conectar a la base de datos", error);
-      }
-    }
-  }
-
-  async getTestData() { 
-      try {
-          this.testmsg = await this.apiService.obtenerDatosTest();
-          console.log(this.testmsg);
-        } catch (error) {
-          console.error("Error al conectar a la base de datos", error);
-        }
-    } 
-}
+export class HomeComponent {}
